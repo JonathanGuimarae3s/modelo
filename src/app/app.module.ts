9aa188e6bb7285cfddecab6b5a2c026cba80552a
Lucas Lemos <balahallslemos@gmail.com>
2023-10-09 09:42:12 -0300

    Paginas componentes concluidas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { SpinerComponent } from './components/spinner/spiner.component';
+import { AboutComponent } from './components/about/about.component';
+import { DestinationComponent } from './components/destination/destination.component';
+import { PackageComponent } from './components/package/package.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,FooterComponent,HeaderComponent
+    HomeComponent,FooterComponent,HeaderComponent, TopBarComponent, SpinerComponent, AboutComponent, DestinationComponent, PackageComponent, BookingComponent, TestimonialComponent, LoginComponent
   ],
   imports: [
     BrowserModule,
