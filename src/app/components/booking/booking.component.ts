cbd45656cf1478958476f3b5c471d6b93925dadd
Lucas Lemos <balahallslemos@gmail.com>
2023-10-11 09:49:42 -0300

    components

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-booking',
@@ -7,5 +7,5 @@ import { Component, Input } from '@angular/core';
 })
 export class BookingComponent {
 
-  @Input() bookingg =  'booking';
+  
 }
