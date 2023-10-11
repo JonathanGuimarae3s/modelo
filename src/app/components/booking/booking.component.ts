5e369c0d81c8044b760b90f9f6d499e401c95bbf
Lucas Lemos <balahallslemos@gmail.com>
2023-10-11 00:23:46 -0300

    teste para ver se est certo dessa maneira

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-booking',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class BookingComponent {
 
+  @Input() bookingg =  'booking';
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  
   public frase: string = 'Descubra a beleza em sua forma mais pura com a Modelo: onde o estilo se encontra com a gratuidade, tornando a sua transformação pessoal ainda mais deslumbrante!';
 }
